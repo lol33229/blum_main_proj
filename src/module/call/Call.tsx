8e554369ddec7edad203c7518d53a8a62cff955a
antspring <max.verzilov32@gmail.com>
2025-01-11 23:57:41 +0500

    deleted /public

--- src/module/call/Call.tsx
+++ src/module/call/Call.tsx
@@ -6,7 +6,7 @@ export default function Call({ }: Props) {
             <div className='w-full flex items-center justify-center'>
                 <div
                     className='bg-cover bg-center bg-no-repeat rounded-[40px] max-w-[1258px] w-full h-[606px] pt-[92px]'
-                    style={{ backgroundImage: 'url("/public/image/1.png")' }}
+                    style={{ backgroundImage: 'url("/image/1.png")' }}
                 >
                     <h1 className='font-normal text-[40px] text-[#FFFFFF] uppercase text-center'>Откройте новые горизонты для вашего бизнеса</h1>
                     <div className='flex items-center justify-center mt-[40px]'>
